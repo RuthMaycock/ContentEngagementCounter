75c42393e7b003b0b04ab35d7bfbf7d1cc3bba0b
RuthMaycock <975808@swansea.ac.uk>
2022-01-16 18:21:20 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.LikeButton = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LikeButton
+        '
+        Me.LikeButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.LikeButton.Location = New System.Drawing.Point(1, 3)
+        Me.LikeButton.Name = "LikeButton"
+        Me.LikeButton.Size = New System.Drawing.Size(379, 220)
+        Me.LikeButton.TabIndex = 0
+        Me.LikeButton.Text = "Like"
+        Me.LikeButton.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(386, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(205, 220)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Comment Like"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(597, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(191, 220)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Comment"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(253, 378)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(290, 60)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Restart"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button4.Location = New System.Drawing.Point(12, 229)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(235, 209)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Rewatch"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button5.Location = New System.Drawing.Point(549, 229)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(239, 209)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Skip"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button6.Location = New System.Drawing.Point(253, 229)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(290, 143)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "Share"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.LikeButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents LikeButton As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+
+    Dim Likes = 0
+    Dim Comment = 0
+    Dim CommentLike = 0
+    Dim Share = 0
+    Dim Rewatch = 0
+    Dim Skip = 0
+    Dim output As String
+
+    Sub Restart()
+        'Having problems with the line below, error reads: System.InvalidCastException: 'Conversion from string "Likes: " to type 'Double' is not valid.'
+        output = "Likes: " + Likes + "Comments: " + Comment + "Comment Likes: " + CommentLike + "Shares: " + Share + "Rewatches: " + Rewatch + "Skips: " + Skip
+        Console.WriteLine(output)
+        Likes = 0
+        Comment = 0
+        CommentLike = 0
+        Share = 0
+        Rewatch = 0
+        Skip = 0
+    End Sub
+    Private Sub LikeButton_Click(sender As Object, e As EventArgs) Handles LikeButton.Click
+        Likes += 1
+    End Sub
+
+    Private Sub CommentLike_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CommentLike += 1
+    End Sub
+
+    Private Sub Share_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Share += 1
+    End Sub
+
+    Private Sub Comment_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Comment += 1
+    End Sub
+
+    Private Sub Rewatch_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Rewatch += 1
+    End Sub
+
+    Private Sub Skip_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Skip += 1
+    End Sub
+
+    Private Sub Restart_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Restart()
+    End Sub
+End Class
